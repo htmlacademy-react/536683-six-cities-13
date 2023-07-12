fec9a9e15478fbd2373a27d50c2c3b8986a281f3
scythecult <scythecult@gmail.com>
2023-07-12 20:19:29 +0300

    -создаёт и добавляет в разметку компонент logo

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import styles from './not-found-page.module.css';
 import { AppRoute } from '../../const';
+import { Logo } from '../../components/logo/logo';
 
 const NotFoundPage = () => (
   <div className={`page ${styles['page--not-found']}`}>
@@ -8,15 +9,7 @@ const NotFoundPage = () => (
       <div className="container">
         <div className="header__wrapper">
           <div className="header__left">
-            <a className="header__logo-link" href="main.html">
-              <img
-                className="header__logo"
-                src="img/logo.svg"
-                alt="6 cities logo"
-                width={81}
-                height={41}
-              />
-            </a>
+            <Logo />
           </div>
           <nav className="header__nav">
             <ul className="header__nav-list">
