6fef7217dbc12d2cf265c67fbaee9f284f9e7ef9
scythecult <scythecult@gmail.com>
2023-07-16 14:51:31 +0300

    -заводит и типизирует моковые данные предложений

---
+++ src/mocks/offers.ts
@@ -0,0 +1,120 @@
+type CityLocation = {
+  latitude: number;
+  longitude: number;
+  zoom: number;
+};
+
+type City = {
+  name: string;
+  location: CityLocation;
+};
+
+type Offer = {
+  id: string;
+  title: string;
+  type: string;
+  price: number;
+  previewImage: string;
+  city: City;
+  location: CityLocation;
+  isFavorite: boolean;
+  isPremium: boolean;
+  rating: number;
+};
+
+const OFFERS: Offer[] = [
+  {
+    id: '6cedd5f6-ca3e-42ff-9bad-7f1c4f52f5a9',
+    title: 'Wood and stone place',
+    type: 'apartment',
+    price: 163,
+    previewImage: 'https://13.design.pages.academy/static/hotel/5.jpg',
+    city: {
+      name: 'Paris',
+      location: {
+        latitude: 48.85661,
+        longitude: 2.351499,
+        zoom: 13,
+      },
+    },
+    location: {
+      latitude: 48.868610000000004,
+      longitude: 2.342499,
+      zoom: 16,
+    },
+    isFavorite: false,
+    isPremium: false,
+    rating: 2.4,
+  },
+  {
+    id: '161281b0-e649-475f-b5cf-f3753d69ef31',
+    title: 'Penthouse, 4-5 rooms + 5 balconies',
+    type: 'house',
+    price: 169,
+    previewImage: 'https://13.design.pages.academy/static/hotel/2.jpg',
+    city: {
+      name: 'Paris',
+      location: {
+        latitude: 48.85661,
+        longitude: 2.351499,
+        zoom: 13,
+      },
+    },
+    location: {
+      latitude: 48.858610000000006,
+      longitude: 2.330499,
+      zoom: 16,
+    },
+    isFavorite: false,
+    isPremium: true,
+    rating: 1.4,
+  },
+  {
+    id: '2186b3e2-db83-4627-be97-2632937550c7',
+    title: 'Wood and stone place',
+    type: 'hotel',
+    price: 192,
+    previewImage: 'https://13.design.pages.academy/static/hotel/17.jpg',
+    city: {
+      name: 'Paris',
+      location: {
+        latitude: 48.85661,
+        longitude: 2.351499,
+        zoom: 13,
+      },
+    },
+    location: {
+      latitude: 48.834610000000005,
+      longitude: 2.335499,
+      zoom: 16,
+    },
+    isFavorite: false,
+    isPremium: false,
+    rating: 3.5,
+  },
+  {
+    id: '68eabb8a-fa4a-400d-8826-5f3039a37707',
+    title: 'Canal View Prinsengracht',
+    type: 'hotel',
+    price: 453,
+    previewImage: 'https://13.design.pages.academy/static/hotel/19.jpg',
+    city: {
+      name: 'Paris',
+      location: {
+        latitude: 48.85661,
+        longitude: 2.351499,
+        zoom: 13,
+      },
+    },
+    location: {
+      latitude: 48.85761,
+      longitude: 2.358499,
+      zoom: 16,
+    },
+    isFavorite: false,
+    isPremium: true,
+    rating: 3.8,
+  },
+];
+
+export { OFFERS };
