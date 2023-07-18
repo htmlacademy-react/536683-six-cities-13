0c2ba5673f9ecbd16d5022c36c5eec6b4652bf0b
scythecult <scythecult@gmail.com>
2023-07-18 23:17:43 +0300

    -создаёт и подключает компонент review-form-rating, review-form

---
+++ src/components/review-form/review-form.tsx
@@ -0,0 +1,74 @@
+import { ReviewRating } from './review-form-rating';
+import { SyntheticEvent, useState } from 'react';
+
+export type TReviewForm = {
+  rating: number;
+  comment: string;
+};
+
+const ReviewForm = () => {
+  const [reviewInfo, setReviewInfo] = useState<TReviewForm>({
+    rating: 0,
+    comment: '',
+  });
+  const isSubmitDisabled = Boolean(
+    reviewInfo.comment.length < 60 || reviewInfo.rating <= 0
+  );
+
+  const handleRatingChange = (rating: number) => {
+    setReviewInfo((prevReviewInfo) => ({ ...prevReviewInfo, rating }));
+  };
+
+  const handleCommentChange = (evt: SyntheticEvent) => {
+    if (evt.target instanceof HTMLTextAreaElement) {
+      const comment = evt.target.value;
+
+      setReviewInfo((prevReviewInfo) => ({ ...prevReviewInfo, comment }));
+    }
+  };
+
+  const handleFormSubmit = (evt: SyntheticEvent) => {
+    evt.preventDefault();
+
+    // eslint-disable-next-line no-console
+    console.log(reviewInfo);
+  };
+
+  return (
+    <form
+      className="reviews__form form"
+      action="#"
+      method="post"
+      onSubmit={handleFormSubmit}
+    >
+      <label className="reviews__label form__label" htmlFor="review">
+        Your review
+      </label>
+      <ReviewRating onRatingChange={handleRatingChange} />
+      <textarea
+        className="reviews__textarea form__textarea"
+        id="review"
+        name="review"
+        placeholder="Tell how was your stay, what you like and what can be improved"
+        value={reviewInfo.comment}
+        onChange={handleCommentChange}
+      />
+      <div className="reviews__button-wrapper">
+        <p className="reviews__help">
+          To submit review please make sure to set{' '}
+          <span className="reviews__star">rating</span> and describe your stay
+          with at least <b className="reviews__text-amount">50 characters</b>.
+        </p>
+        <button
+          className="reviews__submit form__submit button"
+          type="submit"
+          disabled={isSubmitDisabled}
+        >
+          Submit
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export { ReviewForm };
